490baf0952f2e374bc865893a95ab5b5bafb0057
Amit Garu <agaru@uwyo.edu>
2025-11-11 10:16:55 -0700

    updates

--- try_pyepmd.py
+++ try_pyepmd.py
@@ -8,5 +8,5 @@ logging.basicConfig(level=logging.DEBUG)
 # You can use a custom EPMD port, for example 4370, to not interfere with a regular EPMD possibly running
 # Using a custom EPMD port requires running Erlang with environment containing ERL_EPMD_PORT=4370
 # in Linux/MacOS this would look like: ERL_EPMD_PORT=4370 erl
-server = Epmd(port=4369)
+server = Epmd(port=4370)
 asyncio.run(server.start_server())
